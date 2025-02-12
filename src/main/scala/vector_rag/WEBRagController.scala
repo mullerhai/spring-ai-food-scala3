e253cb9c1652b718065850f1c9fffbd796ee3a4a
DESKTOP-I12M6AR\jeffsyry <hnzhang@honhihb.com>
2025-02-12 20:07:09 +0800

    fix scala invoke Java Consumer and Function with lambda  add  extension

--- src/main/scala/vector_rag/WEBRagController.scala
+++ src/main/scala/vector_rag/WEBRagController.scala
@@ -2,6 +2,7 @@ package vector_rag
 
 import io.milvus.client.MilvusServiceClient
 import org.springframework.ai.chat.client.ChatClient
+import org.springframework.ai.chat.client.ChatClient.{ChatClientRequestSpec, PromptUserSpec}
 import org.springframework.ai.chat.client.advisor.*
 import org.springframework.ai.chat.memory.ChatMemory
 import org.springframework.ai.chat.messages.UserMessage
@@ -14,13 +15,13 @@ import org.springframework.ai.rag.retrieval.search.{DocumentRetriever, VectorSto
 import org.springframework.ai.vectorstore.VectorStore
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.{GetMapping, PostMapping, RequestParam, RestController}
-
-import java.util.function.{BiFunction, Consumer, Function}
+import scala.collection.JavaConverters.*
 import java.util
 import java.util.function.Consumer
 import java.util.stream.Collectors
+import java.util.function.{BiFunction, Consumer, Function}
 import scala.jdk.FunctionConverters.*
-import scala.jdk.FunctionWrappers.{AsJavaBiFunction, AsJavaFunction}
+import scala.jdk.FunctionWrappers.{AsJavaBiFunction, AsJavaConsumer, AsJavaFunction}
 
 @RestController class WEBRagController {
   @Autowired private val chatClient: ChatClient = null
@@ -54,8 +55,10 @@ import scala.jdk.FunctionWrappers.{AsJavaBiFunction, AsJavaFunction}
                        Follow Up Input: {question}
                        Standalone question:
                        """
-    def queryTranfunc(query: Query) : Query = {
-      def userFunc(user: ChatClient.PromptUserSpec):Unit= user.text(condenseQuestionPromt).param("question", query.text).param("chat_history", chatHistory.stream.collect(Collectors.joining("\n")))
+
+    def queryTranfunc(query: Query): Query = {
+      def userFunc(user: ChatClient.PromptUserSpec): Unit = user.text(condenseQuestionPromt).param("question", query.text).param("chat_history", chatHistory.stream.collect(Collectors.joining("\n")))
+
       val input = chatClient.prompt
         .user(us => userFunc(us))
         .call.content
@@ -72,21 +75,111 @@ import scala.jdk.FunctionWrappers.{AsJavaBiFunction, AsJavaFunction}
                     """
       val template = new PromptTemplate(answerPrompt)
       template.add("question", query.text)
-//      template.add("context", documents.map((doc: Document) => doc.getContent).mkString("\n\n"))
+      //      template.add("context", documents.map((doc: Document) => doc.getContent).mkString("\n\n"))
       template.add("context", documents.stream.map((doc: Document) => doc.getContent).collect(Collectors.joining("\n\n")))
       new Query(template.render)
     }
 
-//    Function[Query, Query] queryTranfunc = queryTranfunc.asJavaFunction
-//    val  queryTranfun:Function[Query, Query] = queryTranfunc.asJavaFunction
-//    val queryDocfun:BiFunction[Query, util.List[Document], Query] = queryDocFunc.asJavaBiFunction
+    //    Function[Query, Query] queryTranfunc = queryTranfunc.asJavaFunction
+    //    val  queryTranfun:Function[Query, Query] = queryTranfunc.asJavaFunction
+    //    val queryDocfun:BiFunction[Query, util.List[Document], Query] = queryDocFunc.asJavaBiFunction
     val rag = RetrievalAugmentationAdvisor.builder()
       .documentRetriever(documentRetriever)
-      .queryTransformers((q:Query) =>queryTranfunc(q))
-      .queryAugmenter((q,doc) =>queryDocFunc(q,doc))
+      .queryTransformers((q: Query) => queryTranfunc(q))
+      .queryAugmenter((q, doc) => queryDocFunc(q, doc))
       .build
     val answer = chatClient.prompt.user(question).advisors(rag).call.content
     answer
+  }
+
+
+  @PostMapping(Array("/rag3")) def rag3(@RequestParam question: String, @RequestParam(defaultValue = "") chatHistory: util.List[String]): AnyRef = {
+
+    println("use rag3")
+    // https://python.langchain.com/docs/expression_language/cookbook/retrieval#conversational-retrieval-chain
+    val documentRetriever = VectorStoreDocumentRetriever.builder.vectorStore(vectorStore).build
+    val condenseQuestionPromt =
+      """
+                       Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question, in its original language.
+
+                       Chat History:
+                       {chat_history}
+                       Follow Up Input: {question}
+                       Standalone question:
+                       """
+
+    def queryTranfunc(query: Query): Query = {
+      def userFunc(user: ChatClient.PromptUserSpec): Unit = user.text(condenseQuestionPromt).param("question", query.text).param("chat_history", chatHistory.stream.collect(Collectors.joining("\n")))
+
+      val input = chatClient.prompt
+        .user(us => userFunc(us))
+        .call.content
+      new Query(input)
+    }
+
+    def queryDocFunc(query: Query, documents: util.List[Document]): Query = {
+      val answerPrompt =
+        """
+                    Answer the question based only on the following context:
+                    {context}
+
+                    Question: {question}
+                    """
+      val template = new PromptTemplate(answerPrompt)
+      template.add("question", query.text)
+      //      template.add("context", documents.map((doc: Document) => doc.getContent).mkString("\n\n"))
+      template.add("context", documents.stream.map((doc: Document) => doc.getContent).collect(Collectors.joining("\n\n")))
+      new Query(template.render)
+    }
+
+    val queryTransformerFunc =  (query: Query) => {
+          val userFunc = (user: ChatClient.PromptUserSpec) => user.text(condenseQuestionPromt).param("question", query.text)
+            .param("chat_history", chatHistory.stream.collect(Collectors.joining("\n")))
+          val input = chatClient.prompt
+            .withUser(user => user.text(condenseQuestionPromt).param("question", query.text)
+              .param("chat_history", chatHistory.stream.collect(Collectors.joining("\n"))))
+//            .user(userFunc.asInstanceOf[Consumer[ChatClient.PromptUserSpec]])
+            .call.content
+          new Query(input)
+        }
+
+    val queryAugmenterFunc = (query: Query, documents: List[Document]) => {
+          val answerPrompt =
+            """
+                        Answer the question based only on the following context:
+                        {context}
+
+                        Question: {question}
+                        """
+          val template = new PromptTemplate(answerPrompt)
+          template.add("question", query.text)
+          template.add("context", documents.map((doc: Document) => doc.getContent).map(_.mkString("\n\n")) )
+      //.collect(Collectors.joining("\n\n")))
+//          template.add("context", documents.map((doc: Document) => doc.getContent).collect(Collectors.joining("\n\n")))
+          new Query(template.render)
+        } //.asJavaBiFunction[Query,util.List[Document],Query]
+//    val queryTransformerFunc : QueryTransformer = (query: Query) => {
+//          def userFunc(user: ChatClient.PromptUserSpec):Unit= user.text(condenseQuestionPromt).param("question", query.text).param("chat_history", chatHistory.stream.collect(Collectors.joining("\n")))
+//          val input = chatClient.prompt
+//            .user(us => userFunc(us))
+//            .call.content
+//          new Query(input)
+//        }
+    //    Function[Query, Query] queryTranfunc = queryTranfunc.asJavaFunction
+    //    val  queryTranfun:Function[Query, Query] = queryTranfunc.asJavaFunction
+    //    val queryDocfun:BiFunction[Query, util.List[Document], Query] = queryDocFunc.asJavaBiFunction
+//    val rag = RetrievalAugmentationAdvisor.builder().documentRetriever(documentRetriever).withQueryAugmenter(queryAugmenterFunc).withQueryTransformer(queryTransformerFunc).build()
+    val rag = RetrievalAugmentationAdvisor.builder().documentRetriever(documentRetriever).withQueryTransformer(queryTransformerFunc).withQueryAugmenter(queryAugmenterFunc).build()
+
+    //      .documentRetriever(documentRetriever).queryTransformers(queryTransformerFunc).queryAugmenter(queryAugmenterFunc).build
+//      .queryTransformers((q: Query) => queryTranfunc(q))
+//      .queryAugmenter((q, doc) => queryDocFunc(q, doc))
+//      .build
+    val answer = chatClient.prompt.user(question).advisors(rag).call.content
+    answer
+  }
+
+
 
     //    val userFunc = (user: ChatClient.PromptUserSpec) => user.text(condenseQuestionPromt).param("question", query.text).param("chat_history", chatHistory.stream.collect(Collectors.joining("\n")))
     //
@@ -147,7 +240,7 @@ import scala.jdk.FunctionWrappers.{AsJavaBiFunction, AsJavaFunction}
 
 
 
-  }
+//  }
 
   @PostMapping(Array("/chat")) def chat(@RequestParam query: String, @RequestParam conversationId: String): AnyRef = {
     val msgChatMemAdvisor = new MessageChatMemoryAdvisor(chatMemory)
@@ -162,4 +255,44 @@ import scala.jdk.FunctionWrappers.{AsJavaBiFunction, AsJavaFunction}
       .user(query).call.content
     answer
   }
+
+
+//@PostMapping(Array("/chat2")) def chat2(@RequestParam query: String, @RequestParam conversationId: String): AnyRef = {
+//  val msgChatMemAdvisor = new MessageChatMemoryAdvisor(chatMemory)
+//
+////  def advisorFunc(advisor: ChatClient.AdvisorSpec): Unit = advisor
+////    .param(AbstractChatMemoryAdvisor.CHAT_MEMORY_CONVERSATION_ID_KEY, conversationId)
+//
+//  val advisorFunc = (advisor: ChatClient.AdvisorSpec) => advisor
+//        .param(AbstractChatMemoryAdvisor.CHAT_MEMORY_CONVERSATION_ID_KEY, conversationId)
+//  //advisorFunc.asInstanceOf[Consumer[ChatClient.AdvisorSpec]]
+//  val answer = chatClient.prompt
+//    .advisors(msgChatMemAdvisor)
+//    .advisors(ad => advisorFunc(ad))
+//    .user(query).call.content
+//  answer
+//}
+
+
 }
+extension (builder: RetrievalAugmentationAdvisor.Builder)
+  def withQueryTransformer(f: Query => Query): RetrievalAugmentationAdvisor.Builder =
+    builder.queryTransformers((q: Query) =>f(q))
+//    builder.queryTransformers(f.asJavaFunction)
+  def withQueryAugmenter(f: (Query, List[Document]) => Query): RetrievalAugmentationAdvisor.Builder =
+    builder.queryAugmenter((query: Query, doc: util.List[Document]) => f(query, doc.asScala.toList))
+//    builder.queryAugmenter(f.asJavaBiFunction)
+
+//  def withQueryAugmenter(f: Query => Query): RetrievalAugmentationAdvisor.Builder = builder.queryAugmenter(f.asJavaFunction)
+
+//  def documentRetriever(documentRetriever: DocumentRetriever): RetrievalAugmentationAdvisor.Builder = builder.documentRetriever(documentRetriever)
+//  def withQueryTransformers(queryTransformers: java.util.function.Function[Query, Query]): RetrievalAugmentationAdvisor.Builder = builder.queryTransformers(queryTransformers)
+//  def queryAugmenter(queryAugmenter: java.util.function.BiFunction[Query, util.List[Document], Query]): RetrievalAugmentationAdvisor.Builder = builder.queryAugmenter(queryAugmenter)
+//  def queryAugmenter(queryAugmenter: java.util.function.Function[Query, Query]): RetrievalAugmentationAdvisor.Builder = builder.queryAugmenter(queryAugmenter)
+//  def queryAugmenter(queryAugmenter: java.util.function.BiFunction[Query, util.List[Document], Query]): RetrievalAugmentationAdvisor.Builder = builder.queryAugmenter(queryAugmenter)
+//  def queryAugmenter(queryAugmenter: java.util.function.Function[Query, Query]): RetrievalAugmentationAdvisor.Builder = builder.queryAugmenter(queryAugmenter)
+
+extension (request: ChatClientRequestSpec)
+  def withUser(prompt:PromptUserSpec => Unit):ChatClientRequestSpec =
+    request.user(prompt.asJavaConsumer)
+//    request.user(prompt.asInstanceOf[Consumer[ChatClient.PromptUserSpec]])
